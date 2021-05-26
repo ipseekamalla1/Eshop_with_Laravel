90c47a74a108f44f0b66076b61379f441a325d8b
Your Name <you@example.com>
2021-05-26 18:20:13 +0545

    Input and Select component created for admin create product

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -1,15 +1,24 @@
 <form action="/admin/products/store" method="POST">
     @csrf
+    <x-forms.input type="text" name="full_ name" />
     Product Name: <input type="text" name="product_name" id=""><br><br>
     Product Desc: <textarea name="product_desc" id="" cols=30 rows=10></textarea><br>
     Price: <input type="text" name="price" id=""><br><br>
     Category:
-    <select name="category_id" id="">
+    <x-forms.select name="category_id">
         <option value="0"> Select a category</option>
         @foreach ($categories as $category)
-        <option value="{{ $category->id }}">{{ $category->category_name}}</option>   
+            <option value="{{ $category->id }}">{{ $category->category_name }}</option>
         @endforeach
-    </select>
+    </x-forms.select>
+
+
+    {{-- <select name="category_id" id="">
+        <option value="0"> Select a category</option>
+        @foreach ($categories as $category)
+            <option value="{{ $category->id }}">{{ $category->category_name }}</option>
+        @endforeach
+    </select> --}}
     <br><br>
     <input type="Submit" name="Submit" value="Save">
 </form>
